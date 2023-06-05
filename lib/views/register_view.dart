1ebd2b3521582e9d773a1576bd4783b6fd67b8b1
dragonman164 <sanidhiyafirefox123@gmail.com>
2023-06-05 21:49:29 +0530

    Notes Routes

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import '../firebase_options.dart';
+import 'dart:developer' as devtools show log;
+
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -66,15 +66,15 @@ class _RegisterViewState extends State<RegisterView> {
                       email: email,
                       password: password
                   );
-                  print(userCredential);
+                  devtools.log(userCredential.toString());
                 }on FirebaseAuthException catch(e){
                   if(e.code == 'weak-password'){
-                    print("Weak Password");
+                    devtools.log("Weak Password");
                   }else if(e.code == 'email-already-in-use'){
-                    print("Email Already in use");
+                    devtools.log("Email Already in use");
                   }
                   else if(e.code == 'invalid-email'){
-                    print("Invalid Email");
+                    devtools.log("Invalid Email");
                   }
                 }
                 catch(e){
